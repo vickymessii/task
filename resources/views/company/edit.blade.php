866669c00ff44f3cca6c0c4306c40e0fd0687ad9
vickymessii <vikramgill563@gmail.com>
2019-07-27 21:45:27 +0500

    Company CRUD Completed

--- resources/views/company/edit.blade.php
+++ resources/views/company/edit.blade.php
@@ -10,13 +10,13 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Company Create</div>
+                <div class="card-header">Company Update</div>
                 <div class="card-body">
-        <form action="{{ route('company:store') }}" method="POST" enctype="multipart/form-data">
+        <form action="{{ route('company:update', $company) }}" method="POST" enctype="multipart/form-data">
             @csrf
         <div class="form-group">
           <label for="exampleInputEmail1">Name</label>
-          <input type="text" class="form-control" name="name" placeholder="Name">
+          <input type="text" class="form-control" value="{{ $company->name }}" name="name" placeholder="Name">
           @error('name')
           <span class="invalid-feedback" role="alert">
               <strong>{{ $message }}</strong>
@@ -25,7 +25,7 @@
         </div>
         <div class="form-group">
                 <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" placeholder="Enter email">
+                <input type="email" value="{{ $company->email }}" class="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" placeholder="Enter email">
                 @error('email')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -34,6 +34,9 @@
             </div>
               <div class="form-group">
                 <label for="exampleInputPassword1">Logo</label>
+                <div class="card">
+                    <img src="{{ asset('storage/'.$company->logo) }}" alt="">
+                </div>
                 <input type="file" class="form-control" name="logo">
                 @error('logo')
                 <span class="invalid-feedback" role="alert">
@@ -43,7 +46,7 @@
               </div>
               <div class="form-group">
                     <label for="exampleInputPassword1">Website</label>
-                    <input type="text" class="form-control" name="website">
+                    <input type="text" value="{{ $company->website }}" class="form-control" name="website">
                     @error('website')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
--- resources/views/company/show.blade.php
+++ resources/views/company/show.blade.php
@@ -10,30 +10,31 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Company Create</div>
+                <div class="card-header">Company Show</div>
                 <div class="card-body">
-        <form action="{{ route('company:store') }}" method="POST" enctype="multipart/form-data">
-            @csrf
+        <form>
         <div class="form-group">
           <label for="exampleInputEmail1">Name</label>
-          <input type="text" class="form-control" name="name" placeholder="Name">
+          <input type="text" class="form-control" disabled value="{{ $company->name }}" name="name" placeholder="Name">
+         
         </div>
         <div class="form-group">
                 <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" placeholder="Enter email">
+                <input type="email" disabled value="{{ $company->email }}" class="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" placeholder="Enter email">
               
             </div>
               <div class="form-group">
                 <label for="exampleInputPassword1">Logo</label>
-                <input type="file" class="form-control" name="logo">
-               
+                <div class="card">
+                    <img src="{{ asset('storage/'.$company->logo) }}" alt="">
+                </div>
               </div>
               <div class="form-group">
                     <label for="exampleInputPassword1">Website</label>
-                    <input type="text" class="form-control" name="website">
-                   
+                    <input type="text" value="{{ $company->website }}" disabled class="form-control" name="website">
+               
                   </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+                  <a href="{{ url()->previous() }}" class="btn btn-primary">Back</a>
         </form>
     </div>
 </div>
